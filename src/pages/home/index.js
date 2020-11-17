4ff62207f9062c6a0a392ca8c0951c3feadef698
Ruben Grillaert <grillaertr@delawareconsulting.com>
2020-11-17 11:20:17 +0100

    Webcam and Upload identification works

---
+++ src/pages/home/index.js
@@ -0,0 +1,46 @@
+import React, { Fragment, useState } from "react";
+import ReactDOM from "react-dom";
+import { Camera } from "./components/camera/index";
+import { Root, Preview, Footer, GlobalStyle } from "./styles";
+
+function Home() {
+  const [isCameraOpen, setIsCameraOpen] = useState(false);
+  const [cardImage, setCardImage] = useState();
+
+  return (
+    <Fragment>
+      <Root>
+        {isCameraOpen && (
+          <Camera
+            onCapture={(blob) => setCardImage(blob)}
+            onClear={() => setCardImage(undefined)}
+          />
+        )}
+
+        {cardImage && (
+          <div>
+            <h2>Preview</h2>
+            <Preview src={cardImage && URL.createObjectURL(cardImage)} />
+          </div>
+        )}
+
+        <Footer>
+          <button onClick={() => setIsCameraOpen(true)}>Open Camera</button>
+          <button
+            onClick={() => {
+              setIsCameraOpen(false);
+              setCardImage(undefined);
+            }}
+          >
+            Close Camera
+          </button>
+        </Footer>
+      </Root>
+      <GlobalStyle />
+    </Fragment>
+  );
+}
+
+export default Home;
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
